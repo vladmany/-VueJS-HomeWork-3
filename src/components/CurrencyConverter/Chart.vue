f212109446ddb53ba0f7224081dbffdc11a98254
vladmany <46404862+vladmany@users.noreply.github.com>
2020-02-14 18:33:26 +0200

    Add files via upload

---
+++ src/components/CurrencyConverter/Chart.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="chart">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Chart",
+        props: [],
+        data() {
+            return {
+                
+            }
+        },
+        methods: {
+        }
+    }
+</script>
+
+<style scoped>
+    .chart {
+        max-width: 500px;
+    }
+</style>
